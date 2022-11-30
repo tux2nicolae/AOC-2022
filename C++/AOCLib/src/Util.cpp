377cd6cc625b42e5799f3b4826f2c646e732beca
Telechi Nicolae <tux2nicolae@gmail.com>
2022-11-30 21:45:15 +0200

    Add initial setup

---
+++ C++/AOCLib/src/Util.cpp
@@ -0,0 +1,6 @@
+/**
+ * Advent of code 2022
+ * @author : Nicolae Telechi
+ */
+#include "pch.h"
+#include "Util.h"
---
+++ C++/Day01/src/main.cpp
@@ -0,0 +1,41 @@
+/**
+ * Advent of code 2022
+ * @author : Nicolae Telechi
+ */
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <set>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+#include "../../AOCLib/src/Algorithm.h"
+#include "../../AOCLib/src/FStreamReader.h"
+#include "../../AOCLib/src/FStreamWriter.h"
+#include "../../AOCLib/src/Math.h"
+#include "../../AOCLib/src/Time.h"
+
+int main()
+{
+  ifstream in("..\\src\\_input.in");
+  // ofstream out("..\\src\\_output.out");
+
+  FStreamReader reader(in);
+  vector<int>   v = reader.ReadDataAs<int>();
+
+  // part 1
+
+  // out
+  // FStreamWriter writer(out);
+  // writer.WriteVector(v);
+
+  return 0;
+}
