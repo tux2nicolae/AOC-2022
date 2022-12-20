1df2e2ead2befe12e00c52f5133b1d322ce1cc3b
Telechi Nicolae <39991250+tux2nicolae@users.noreply.github.com>
2022-12-20 13:36:56 +0200

    Add solution for part 2

--- C++/Day20/src/main.cpp
+++ C++/Day20/src/main.cpp
@@ -42,15 +42,15 @@ int main()
   ofstream out("..\\src\\_output.out");
 
   FStreamReader reader(in);
-  auto          lines = reader.ReadDataAs<int>();
+  auto          lines = reader.ReadDataAs<long long>();
 
-  vector<pair<int, int>> numbers;
-  for (int i = 0; i < lines.size(); i++)
+  vector<pair<long long, long long>> numbers;
+  for (long long i = 0; i < lines.size(); i++)
   {
-    numbers.push_back({ lines[i], i });
+    numbers.push_back({ lines[i] * 811589153, i });
   }
 
-  auto computeDestination = [&](vector<pair<int, int>>::iterator from, int count)
+  auto computeDestination = [&](vector<pair<long long, long long>>::iterator from, long long count)
   {
     assert(from == begin(numbers) || from == end(numbers) - 1);
 
@@ -60,71 +60,82 @@ int main()
     return begin(numbers) + toPosition;
   };
 
-  for (int i = 0; i < numbers.size(); i++)
+  long long mixCount = 10;
+  while (mixCount--)
   {
-    auto it = find_if(begin(numbers), end(numbers),
-                      [=](pair<int, int> nr)
-                      {
-                        return nr.second == i;
-                      });
-
-    assert(it != end(numbers));
-
-    if (0 <= it->first)
+    for (long long i = 0; i < numbers.size(); i++)
     {
-      auto myNumber = it->first;
+      auto it = find_if(begin(numbers), end(numbers),
+                        [=](pair<long long, long long> nr)
+                        {
+                          return nr.second == i;
+                        });
 
-      // bring to front
-      std::rotate(begin(numbers), it, end(numbers));
-      assert(numbers.front().first == myNumber);
+      assert(it != end(numbers));
 
-      auto from = begin(numbers);
-      auto to   = computeDestination(from, numbers.front().first);
-
-      assert(from <= to);
-      if (from < to)
+      if (0 <= it->first)
       {
-        std::rotate(from, from + 1, to + 1);
-      }
-    }
-    else
-    {
-      auto myNumber = it->first;
+        long long count = it->first % (numbers.size() - 1);
 
-      // bring to back
-      if (it != end(numbers) - 1)
+        // bring to front
+        std::rotate(begin(numbers), it, end(numbers));
+
+        auto current = 0;
+        while (count--)
+        {
+          auto next = current + 1;
+          if (next == numbers.size())
+            next = 0;
+
+          swap(numbers[current], numbers[next]);
+
+          current++;
+          if (current == numbers.size())
+            current = 0;
+        }
+      }
+      else
       {
-        std::rotate(begin(numbers), it + 1, end(numbers));
+        auto count = AOC::Abs(it->first) % (numbers.size() - 1);
+
+        // bring to back
+        if (it != end(numbers) - 1)
+        {
+          std::rotate(begin(numbers), it + 1, end(numbers));
+        }
+
+        long long current = numbers.size() - 1;
+        while (count--)
+        {
+          auto prev = current - 1;
+          if (prev == -1)
+            prev = numbers.size() - 1;
+
+          swap(numbers[current], numbers[prev]);
+
+          current--;
+          if (current < 0)
+            current = numbers.size() - 1;
+        }
       }
 
-      assert((numbers.end() - 1)->first == myNumber);
+      // for (long long i = 0; i < numbers.size(); i++)
+      //{
+      //   out << numbers[i].first << " ";
+      // }
 
-      auto from = computeDestination(end(numbers) - 1, numbers.back().first);
-      auto to   = end(numbers) - 1;
-
-      assert(from <= to);
-      if (from < to)
-      {
-        std::rotate(from, to, to + 1);
-      }
+      // out << endl;
     }
-
-    for (int i = 0; i < numbers.size(); i++)
-    {
-      out << numbers[i].first << " ";
-    }
-
-    out << endl;
   }
 
   auto zeroIt = find_if(begin(numbers), end(numbers),
-                        [](pair<int, int> nr)
+                        [](pair<long long, long long> nr)
                         {
                           return nr.first == 0;
                         });
 
   auto count = count_if(begin(numbers), end(numbers),
-                        [=](pair<int, int> nr)
+                        [=](pair<long long, long long> nr)
                         {
                           return nr.second == 0;
                         });
@@ -132,17 +143,17 @@ int main()
   assert(count == 1);
   std::rotate(begin(numbers), zeroIt, end(numbers));
 
-  // auto a = numbers[1000].first;
-  // auto b = numbers[2000].first;
-  // auto c = numbers[3000].first;
+  auto a = numbers[1000].first;
+  auto b = numbers[2000].first;
+  auto c = numbers[3000].first;
 
-  // std::cout << endl;
-  // std::cout << a << endl;
-  // std::cout << b << endl;
-  // std::cout << c << endl;
+  std::cout << endl;
+  std::cout << a << endl;
+  std::cout << b << endl;
+  std::cout << c << endl;
 
-  // std::cout << endl;
-  // cout << a + b + c;
+  std::cout << endl;
+  cout << a + b + c;
 
   return 0;
 }
